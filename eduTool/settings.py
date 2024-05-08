3c5501c3231bdbc66206887e27bac26d96dcd907
mima219 <91896775+mima219@users.noreply.github.com>
2024-05-08 12:08:44 +0200

    Add files via upload

--- eduTool/settings.py
+++ eduTool/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'eduTool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500)
     # 'default': dj_database_url.parse()
